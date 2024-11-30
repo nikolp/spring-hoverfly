c23081136feb315fc8c0217060e76b450d6f659b
Philip Nikolov <philip.k.nikolov@gmail.com>
2024-11-30 18:53:36 -0500

    lint

--- serviceA/src/test/java/com/example/serviceA/IntegrationTest.java
+++ serviceA/src/test/java/com/example/serviceA/IntegrationTest.java
@@ -1,7 +1,5 @@
 package com.example.serviceA;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
