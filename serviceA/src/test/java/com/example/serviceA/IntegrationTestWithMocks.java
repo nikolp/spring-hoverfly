764f7d4d22ad98aaa0a3c05c9357f4b50b484191
Philip Nikolov <philip.k.nikolov@gmail.com>
2024-11-30 20:01:53 -0500

    simple example

---
+++ serviceA/src/test/java/com/example/serviceA/IntegrationTestWithMocks.java
@@ -0,0 +1,31 @@
+package com.example.serviceA;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+public class IntegrationTestWithMocks {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @MockBean
+    ServiceBClient serviceBClient;
+
+    @Test
+    void testBasic() {
+        when(serviceBClient.getName()).thenReturn("My Friend");
+        ResponseEntity<HelloDto> responseEntity = testRestTemplate.getForEntity("/", HelloDto.class);
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertEquals("Hello My Friend", responseEntity.getBody().getMessage());
+    }
+}
