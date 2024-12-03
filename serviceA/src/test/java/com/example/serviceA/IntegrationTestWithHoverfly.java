3d9f389ac114c50267ef0d267247b1a1752d0dbc
Philip Nikolov <philip.k.nikolov@gmail.com>
2024-12-02 22:19:11 -0500

    remove un-necesary Builder, hoverfly intercepts out of the box without it

--- serviceA/src/test/java/com/example/serviceA/IntegrationTestWithHoverfly.java
+++ serviceA/src/test/java/com/example/serviceA/IntegrationTestWithHoverfly.java
@@ -10,10 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import static io.specto.hoverfly.junit.core.HoverflyConfig.localConfigs;
 import static io.specto.hoverfly.junit.core.HoverflyMode.SIMULATE;
@@ -27,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
                 // since Hoverfly is not configured to intercept localhost
                 // and we do want Hoverfly to intercept calls to serviceB and simulate a response
                 properties = {"serviceB.base_url=http://serviceb"})
-@Import(RestTemplateRoutedThruHoverflyConfig.class)
 @Slf4j
 public class IntegrationTestWithHoverfly {
 
@@ -38,8 +36,7 @@ public class IntegrationTestWithHoverfly {
     private TestRestTemplate testRestTemplate;
 
     @Autowired
-    private RestTemplateBuilder restTemplateBuilderWithHoverfly;
-    private static Hoverfly hoverfly;
+    private RestTemplate restTemplate;
 
     @BeforeAll
     static void setUpHoverfly() {
@@ -51,22 +48,21 @@ public class IntegrationTestWithHoverfly {
         // Turn this ON for hoverfly to intercept calls to localhost
         // However that's a problem since our system under test is running on localhost
         // hoverflyConfig.proxyLocalHost();
-        hoverfly = new Hoverfly(hoverflyConfig, SIMULATE);
+        Hoverfly hoverfly = new Hoverfly(hoverflyConfig, SIMULATE);
         hoverfly.start();
         hoverfly.simulate(source);
     }
     @Test
     @SneakyThrows
     void testBasic() {
-        // Confirm hoverfly wiring works
-        // RestTemplate restTemplateWithHoverfly = restTemplateBuilderWithHoverfly.build();
+        // Confirm hoverfly wiring works directly from here
         // This works
-        // ResponseEntity<String> resp0 = restTemplateWithHoverfly.getForEntity("http://www.baeldung.com/api/courses/1", String.class);
+        // ResponseEntity<String> resp0 = restTemplate.getForEntity("http://serviceb/name", String.class);
         // This also works but to turn it on you have to do localConfigs().proxyLocalHost();
         // But then your calls to system under test will be intercepted making you unable to test it
-        // ResponseEntity<String> resp1 = restTemplateWithHoverfly.getForEntity("http://localhost:8082/foo", String.class);
+        // ResponseEntity<String> resp1 = restTemplate.getForEntity("http://localhost:8082/foo", String.class);
 
-        // Connect to our real app under test
+        // Connect to our real app under test, and it, behind the scenes will call http://serviceb/name
         ResponseEntity<HelloDto> responseEntity = testRestTemplate.getForEntity("/", HelloDto.class);
         assertEquals(HttpStatusCode.valueOf(200), responseEntity.getStatusCode());
         assertEquals("Hello my friend", responseEntity.getBody().getMessage());
